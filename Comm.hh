f6db5559b806d750e14c195576c8303858c0a39d
Robert W. Robey <brobey@ml-fey2.lanl.gov>
2016-01-04 09:04:56 -0700

    Removing unneeded space

--- Comm.hh
+++ Comm.hh
@@ -1,5 +1,5 @@
 /* Copyright 2015.  Los Alamos National Security, LLC. This material was produced
- * under U.S. Government contract DE-AC52-06NA25396 for Los Alamos National 
+ * under U.S. Government contract DE-AC52-06NA25396 for Los Alamos National
  * Laboratory (LANL), which is operated by Los Alamos National Security, LLC
  * for the U.S. Department of Energy. The U.S. Government has rights to use,
  * reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR LOS
